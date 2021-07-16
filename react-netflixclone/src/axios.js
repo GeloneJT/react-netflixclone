3abd7aa66dff43f066e898f1bdb33a92a98227fd
Jelani Thomas <jelani13@icloud.com>
2021-07-16 02:25:42 -0400

    added axios file with axios call

---
+++ react-netflixclone/src/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+/**base url base url to make requests to the movie database */
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.orf/3",
+});
+
+export default instance;
