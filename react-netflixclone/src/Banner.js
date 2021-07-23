230a8bcc53f4d207607f15ac1ad75246f7d37dd8
Jelani Thomas <jelani13@icloud.com>
2021-07-23 02:19:00 -0400

    top banner completed with CSS styling

--- react-netflixclone/src/Banner.js
+++ react-netflixclone/src/Banner.js
@@ -46,6 +46,8 @@ function Banner() {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+      
+      <div className="banner_fadeBottom"></div>
     </header>
   );
 }
